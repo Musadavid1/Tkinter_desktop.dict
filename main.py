ec1d2e190da52d48246f7ea848f8f202c019bc49
tsamya <tsamyamercy8@gmail.com>
2025-01-18 16:10:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+french_dict = {
+    "House" : "Maison" ,
+    "Car" : "Voiture" ,
+    "Book" : "Livre",
+    "Water" : "Eau" ,
+    "Food" : "Nourriture",
+    "Friend" : "Amie",
+    "Love" : "Amour",
+    "Sun" : "Soleil",
+    "Moon" : "Lune",
+    "Tree" : "Arbre",
+    "sky" : "Ciel",
+    "Flower" : "Fleur",
+    "Mountain" : "Montagne",
+    "River" : "Riviere",
+    "City" : "Ville",
+    "Dog" : "Chien",
+    "Cat" : "Chat",
+    "Music" : "Musique",
+    "Family" : "Famille",
+    "Dream" : "Reve",
+}
+print(french_dict)
